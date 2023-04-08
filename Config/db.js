ec56a1d9044d0b5a9e0c308a02e43a14adf392d7
Nikhil Magar <nikhilmagar1995@gmail.com>
2023-04-08 20:22:58 +0530

    Added

--- Config/db.js
+++ Config/db.js
@@ -3,6 +3,6 @@ const dotenv=require("dotenv");
 dotenv.config();
 
 
-const connection=mongoose.connect(process.env.MongoURL)
+const connection=mongoose.connect(process.env.MongoURL,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 module.exports={connection}
